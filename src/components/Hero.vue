3c3d08569becb8f4fc417eacfd2e6d779b8b5576
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-03-25 16:31:21 +0100

    prettier/prettier 16 problem

---
+++ src/components/Hero.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="jumbotron">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Hero",
+  props: {
+    msg: String,
+  },
+};
+</script>
